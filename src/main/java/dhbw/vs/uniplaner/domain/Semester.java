fc6302fc588f18e2cc17428d4dddccab7bb3f42e
Zoidberg3 <manuel@rodri.de>
2021-03-31 12:40:01 +0200

    Semesterklasse neu

--- src/main/java/dhbw/vs/uniplaner/domain/Semester.java
+++ src/main/java/dhbw/vs/uniplaner/domain/Semester.java
@@ -18,8 +18,95 @@ public class Semester implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    
+    @Column(name = "number")
+    private Integer number;
+    
+    @Column(name = "startDate")
+    private LocalDate startDate;
+    
+    @Column(name = "endDate")
+    private LocalDate endDate;
+    
+    @Column(name = "semesterNumber")
+    private Integer semesterNumber;
+    
+    @Column(name = "name")
+    private String name;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Integer getNumber() {
+        return number;
+    }
+    
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    
+    public Integer getSemesterNumber() {
+        return semesterNumber;
+    }
+    
+    public void setSemesterNumber(Integer semesterNumber) {
+        this.semesterNumber = semesterNumber;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Semester number(Integer number){
+        this.number = number;
+        return this;
+    }
+    
+    public Semester startDate(LocalDate startDate){
+        this.startDate = startDate;
+        return this;
+    }
+    
+    public Semester endDate(LocalDate endDate){
+        this.endDate = endDate;
+        return this;
+    }
+    
+    public Semester semesterNumber(Integer semesterNumber){
+        this.semesterNumber = semesterNumber;
+        return this;
+    }
+    
+    public Semester name(String name){
+        this.name = name;
+        return this;
+    }
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) {
